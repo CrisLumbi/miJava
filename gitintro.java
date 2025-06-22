0bb639b446b739613914bd0063c44f37c67df988
Cris Angel <lumbicrisan@gmail.com>
2025-06-21 18:55:16 -0600

    java simple project

---
+++ gitintro.java
@@ -0,0 +1,7 @@
+public class gitintro {
+    // This is a simple Java program to demonstrate a basic structure
+
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+}
+}
